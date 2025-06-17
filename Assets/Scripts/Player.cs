76e354a109896711192fc355a7f36331c31b41f9
semious <semious@gmail.com>
2025-06-17 18:08:08 +0800

    update

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -1,18 +1,40 @@
 using System.Collections;
 using System.Collections.Generic;
+using UnityEditor.Rendering;
 using UnityEngine;
+using static Unity.Burst.Intrinsics.X86.Avx;
 
 public class Player : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    [Header("Movement")]
+    private Vector3 playerVelocity;
+    [SerializeField] private float moveSpeed = 15f;
+    [SerializeField] private float jumpHeight = 2f;
+    [SerializeField] private float gravityValue = -9.81f;
+
+    [SerializeField] private GameObject body;
+    [SerializeField] private GameObject ground;
+
+    private void Start()
     {
-        
     }
 
-    // Update is called once per frame
-    void Update()
+    private void Update()
     {
-        
+        //    Vector2 input = new Vector2(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical"));
+
+        Rigidbody2D rb = body.GetComponent<Rigidbody2D>();
+        rb.velocity = new Vector2(Input.GetAxis("Horizontal"), rb.velocity.y);
+        //controller.Move(transform.TransformDirection(input) * moveSpeed * Time.deltaTime);
+
+        //if (Input.GetButtonDown("Jump"))
+        //{
+        //    playerVelocity.y += Mathf.Sqrt(jumpHeight * -3.0f * gravityValue);
+        //}
+
+        //playerVelocity.y += gravityValue * Time.deltaTime;
+        //controller.Move(playerVelocity * Time.deltaTime);
+
+
     }
 }
